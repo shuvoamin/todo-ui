1ed9935f2a11bc014f84ec57fe55c37a4fdcb632
shuvoamin <shuvoamin@yahoo.com>
2020-06-18 01:00:50 +0100

    crud ops wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiUrl = "https://todoapi-demo.azurewebsites.net/api/todoitems";
+
+const todoList = (props) => {
+  fetch(apiUrl)
+    .then((resp) => console.log(resp.json()));
+}
+
+class Card extends React.Component {
+  render() {
+    //todoList();
+    const todoItem = this.props;
+    return (
+      <div>{todoItem.name}</div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App title="Simple React CRUD Operation" />, 
   document.getElementById('root')
 );
 
